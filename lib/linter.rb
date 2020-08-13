388f700e81617068e37ff7a77b692effd45fc2bf
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-13 07:14:53 +0100

    Add new Linter class

--- lib/linter.rb
+++ lib/linter.rb
@@ -1,42 +1,19 @@
-class Linter
-  def block_indentation(lines, errorArr)
-    lines.each_with_index do |line, index|
-      if line.include?(':')
-        errorArr << "Expected indentation of 2 spaces on line #{index + 1}" unless line.match(/^\s{2}[a-zA-Z]/)
-      end
-    end
-    errorArr
-  end
+require_relative ".lintrules.rb"
 
-  def block_opening_brace_space_before(lines, errorArr)
-    lines.each_with_index do |line, index|
-      if line.include?("{\n")
-        linecheck = line.slice!('{')
-        errorArr << "Expect space before opening brace on #{index + 1}" unless line.end_with?(" \n")
-      end
-    end
+class Linter
+  def initialize(file)
+    @errorsArr = []
+    @lines = File.open(file).to_a
   end
-  
-  def trailing_space(lines, errorArr)
-    lines.each_with_index do |line, index|
-      if line.end_with?("; \n")
-        errorArr << "Trailing space found on line: #{index + 1}" 
-      end
-    end
-  end
-  
-  def length_Zero_no_Unit(lines, errorArr)
-    lines.each_with_index do |line, index|
-      if line.include?(";")
-        errorArr << "Zero values do not need units on line: #{index + 1}" if line.match(/[0](\w|%)/) 
-      end
-    end
-  end
-  
-  def no_extra_semicolons(lines, errorArr)
-    lines.each_with_index do |line, index|
-        errorArr << "Extra semicolons found on line: #{index + 1}" if line.count(";") != 1
-      end
-    end
+  def self.lintcheck(file)
+     if file.positive?
+      runcheck = LintRules.new
+      runcheck.block_indentation(lines, errorArr)
+      runcheck.block_opening_brace_space_before(lines, errorArr)
+      runcheck.trailing_space(lines, errorArr)
+      runcheck.length_Zero_no_Unit(lines, errorArr)
+      no_extra_semicolons(lines, errorArr)
+     end
+     errorArr
   end
 end
