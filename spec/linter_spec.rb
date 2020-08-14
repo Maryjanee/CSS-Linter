5ad6753daf147941e98811b3a3724a3bd008e5c3
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-14 03:33:50 +0100

    Add test for no extra semi-colons

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -3,10 +3,13 @@ require_relative '../lib/linter.rb'
 
 describe 'LintRules' do
   let(:subject) { LintRules.new }
-  let(:zero_unit_rule) { ['p {', 'padding: 0', 'margin: 0px;', '}'] }
-  let(:no_zero_unit_rule) { ['h1 {', 'padding: 0', 'margin: 0;', '}'] }
-  let(:indented_code) { ['.btn {','  padding: 0',   '  margin: 0;', '}'] }
-  let(:not_indented_code) { ['.btn {','padding: 0', 'margin: 0;', '}'] }
+  let(:zero_unit_rule) { ['p {', 'padding: 0;', 'margin: 0px;', '}'] }
+  let(:no_zero_unit_rule) { ['h1 {', 'padding: 0;', 'margin: 0;', '}'] }
+  let(:indented_code) { ['.btn {','  padding: 0;',   '  margin: 0;', '}'] }
+  let(:multiple_colons) { ['.btn {','  padding: 0;;','margin: 0;;', '}'] }
+  let(:not_indented_code) { ['.btn {','padding: 0;', '  margin: 0;', '}'] }
+  let(:trailing_space_rule) { ['p {', 'padding: 0;   ', 'margin: 0px;  ', '}'] }
+  let(:one_trailing_space) { ['p {', 'padding: 0;   ', 'margin: 0px;', '}'] }
   let(:errors) { [] }
   
   describe '#block_indentation' do
@@ -14,12 +17,21 @@ describe 'LintRules' do
       expect(subject.block_indentation(indented_code, errors)).to eql([])
     end
     it 'should return an array with error message when zero units are found on the line' do
-      expect(subject.block_indentation(not_indented_code, errors)).to eql(["Expected indentation of 2 spaces on line 3"])
+      expect(subject.block_indentation(not_indented_code, errors)).to eql(["Expected indentation of 2 spaces on line 2"])
     end
     it 'should return an array with error message when zero units are found on the line' do
       expect(subject.block_indentation(zero_unit_rule, errors)).not_to be([])
     end
-
+  end
+  
+  describe '#no_extra_semicolons' do
+    it 'should return messages on lines where extra semicolons are added' do
+      expect(subject.no_extra_semicolons(multiple_colons, errors)).to eql(["Extra semicolons found on line: 2", "Extra semicolons found on line: 3"])
+    end
+    it 'should return messages on lines where extra semicolons are added' do
+      expect(subject.no_extra_semicolons(indented_code, errors)).to eql([])
+    end
+    
   end
   
   
@@ -36,5 +48,4 @@ describe 'LintRules' do
       expect(subject.length_zero_no_unit(zero_unit_rule, errors)).not_to eql([])
     end
   end
-
 end
