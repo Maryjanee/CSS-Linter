a3878e04aea386e366928e6dd476a19a852dcf16
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-14 04:00:28 +0100

    Update block_opening_brace_space_before method

--- lib/lintrules.rb
+++ lib/lintrules.rb
@@ -1,7 +1,7 @@
 class LintRules
   def block_indentation(lines, error)
     lines.each_with_index do |line, index|
-      if line.include?('{\n')
+      if line.include?(';')
         error << "Expected indentation of 2 spaces on line #{index + 1}" unless line.match(/^\s{2}[a-zA-Z]/)
       end
     end
@@ -11,18 +11,18 @@ class LintRules
   def block_opening_brace_space_before(lines, error)
     lines.each_with_index do |line, index|
       if line.include?("{\n")
-        error << "Expect space before opening brace on #{index + 1}" unless line.end_with?(" \n")
+        error << "Expect space before opening brace on #{index + 1}" if line.match(/(^.|^#)(\w+)(, (\.)*\w+)*(, (\#)*\w+)*(\{\n)/)
       end
     end
     error
   end
 
-  def trailing_space(lines, error)
-    lines.each_with_index do |line, index|
-      error << "Trailing space found on line: #{index + 1}" if line.end_with?("; \n")
-    end
-    error
-  end
+  # def trailing_space(lines, error)
+  #   lines.each_with_index do |line, index|
+  #     error << "Trailing space found on line: #{index + 1}" if line.match("; \n")
+  #   end
+  #   error
+  # end
 
   def length_zero_no_unit(lines, error)
     lines.each_with_index do |line, index|
